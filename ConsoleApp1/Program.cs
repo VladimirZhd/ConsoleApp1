5a845032e0a7e1d37fefda153de6ac682e2c1804
VladimirZhd <zhd18001@byui.edu>
2020-04-23 13:03:56 -0600

    initial commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var name = "Vova Zhdanov";
+            var name = "Vova";
             string location = "Rexburg, ID";
 
             Console.WriteLine($"My name is: {name}");
